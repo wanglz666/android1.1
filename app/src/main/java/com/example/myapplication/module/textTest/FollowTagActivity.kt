0d3fdec10ed7a428f782f10512922b3f56b2522a
wanglizhi <wanglizhi@bizgo.com>
2025-05-22 18:27:19 +0800

    [feat]n行追随标签

--- app/src/main/java/com/example/myapplication/module/textTest/FollowTagActivity.kt
+++ app/src/main/java/com/example/myapplication/module/textTest/FollowTagActivity.kt
@@ -17,13 +17,22 @@ import com.scwang.smart.refresh.layout.util.SmartUtil
 
 
 /**
- * 参考https://juejin.cn/post/6891284263174406157?searchId=2023112710130194F12865E1177E99712D
+ * 参考
+ * https://juejin.cn/post/6891284263174406157?searchId=2023112710130194F12865E1177E99712D
+ * https://tristanzeng.github.io/2019/05/29/TextView%E5%A4%9A%E8%A1%8C%E6%96%87%E5%AD%97%E8%B6%85%E5%87%BA%E6%97%B6%E5%A6%82%E4%BD%95%E5%9C%A8%E7%9C%81%E7%95%A5%E5%8F%B7%E5%90%8E%E6%B7%BB%E5%8A%A0%E5%9B%BE%E6%A0%87/?
  */
 class FollowTagActivity : BaseActivity() {
     private lateinit var binding: ActivityFollowTagBinding
-    var tagSrc = "文字是人类用表意符号记录表达信息以传之久远的方式和工具。现代文字大多是记录语言的工具。人类往往先有口头的语言后产生书面文字，很多小语种，有语言但没有文字。文字的不同体现了国家和民族的书面表达的方式和思维不同。文字使人类进入有历史记录的文明社会。"
+    private var tagSrc = "文字是人类用表意符号记录表达信息以传之久远的方式和工具。现代文字大多是记录语言的工具。人类往往先有口头的语言后产生书面文字，很多小语种，有语言但没有文字。文字的不同体现了国家和民族的书面表达的方式和思维不同。文字使人类进入有历史记录的文明社会。"
 
     override fun initData(savedInstanceState: Bundle?) {
+        // 简单实现单行、两行
+        followTagForDouble()
+        // n行
+        followTagForN()
+    }
+
+    private fun followTagForDouble() {
         binding.tvTagOne.text = tagSrc
         binding.tvTagTwo.text = tagSrc
         //判断第一行是否可以完整显示
@@ -42,13 +51,9 @@ class FollowTagActivity : BaseActivity() {
                 binding.tvTagTwo.text = srcTwoContent
             }
         }
-
-
-        // n行
-        followTag()
     }
 
-    private fun followTag() {
+    private fun followTagForN() {
         binding.tvContent.post { // 计算文字超出部分做精确截取
             var text = "$tagSrc(精)"
             binding.tvContent.text = text
@@ -84,7 +89,7 @@ class FollowTagActivity : BaseActivity() {
     /**
      * 获取textview某行line内容
      */
-    fun getTextLineContent(textView: TextView?, line: Int, completeStr: String?): String {
+    private fun getTextLineContent(textView: TextView?, line: Int, completeStr: String?): String {
         val result = ""
         if (textView == null || completeStr.isNullOrEmpty()) {
             return result
